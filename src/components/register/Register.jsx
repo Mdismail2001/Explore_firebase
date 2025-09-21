56acb4c6ce8bc7ee15153fc0fe7fb802310d728b
md ismail <mdismail85773@gmail.com>
2025-09-21 13:13:21 +0800

    explore firebase

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Register = () => {
+
+    const handleBtn = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
+    return (
+        <form onSubmit={handleBtn}>   {/* ✅ attach here */}
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" name='email' className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" name='password' className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button type="submit" className="btn btn-neutral mt-4">Register</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default Register;
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtRsWiHXVBiW_ZjK6pRy4BMd_QGk4DfYU",
+  authDomain: "explore-firebase-a3faf.firebaseapp.com",
+  projectId: "explore-firebase-a3faf",
+  storageBucket: "explore-firebase-a3faf.firebasestorage.app",
+  messagingSenderId: "879038260960",
+  appId: "1:879038260960:web:41750116e419f46e5b5285"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
