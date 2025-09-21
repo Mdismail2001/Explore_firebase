42e405f59aae3b65417a592b5c536f7a52ea7b1d
md ismail <mdismail85773@gmail.com>
2025-09-21 17:06:22 +0800

    login and register with firebase

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import { auth } from '../../firebase.init';
+import { Link } from 'react-router';
 
 const Login = () => {
+    const [error, setError]= useState('')
+    const [success, setSuccess] = useState(false);
+    const emailRef = useRef();
+
+
+// form function
+    const handleForm =(e)=>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        // console.log(email, password);
+
+        // reset 
+        setSuccess(false)
+        setError('');
+        // login with firebase
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result =>{
+            console.log(result.user)
+            setSuccess(true)
+        })
+        .catch(error=>{
+            // console.log(error.message)
+            setError(error.message)
+        })
+    }
+// for forget password 
+const forgetPassword =()=>{
+    console.log(emailRef.current.value)
+   const email = emailRef.current.value;
+
+    setError('');
+    // send otp for change password
+    sendPasswordResetEmail(auth, email)
+    .then(() =>{
+        alert("send an OTP for for change password please check your email ")
+    })
+    .catch(error =>{
+        setError(error.message);
+    })
+}
+
+
+
     return (
-        <div>
-            <h1>this is login page</h1>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <form onSubmit={handleForm} className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" name='email' ref={emailRef} className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+                <div><a onClick={forgetPassword} className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>New to this website? Please <Link to="/register" className='text-green-700 underline'>Register</Link></p>
+                {
+                    error && <p className='text-red-600'>{error}</p>
+                }
+                {
+                    success && <p className='text-green-500'>User logged in successfully</p>
+                }
+            </div>
+            </div>
         </div>
+        </div>    
     );
 };
 
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,22 +1,44 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from 'firebase/auth';
+import React, {  useState } from 'react';
 import { auth } from '../../firebase.init';
+import { Link } from 'react-router';
 
 const Register = () => {
+    const [error, setError]=useState('')
+    const [success, setSuccess]= useState(false)
 
     const handleBtn = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        // show error and success message
+        setError('')
+        setSuccess(false)
+
         // console.log(email, password);
+        const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+        if (passwordRegExp.test(password) === false) {
+        setError("Password must have at least 1 uppercase, 1 lowercase and be 6+ characters long.");
+        return;
+        }
+
+
+        // create user using firebase
         createUserWithEmailAndPassword(auth,email, password)
         .then(result =>{
-            console.log(result)
+            // console.log(result)
+            // setSuccess(true);
+            // email verify check
+            sendEmailVerification(auth, currentUser)
+            .then(()=>{
+            setSuccess(true);
+            })
         })
         .catch(error =>{
-            console.log(error)
+            setError(error.message)
         })
     }
+
 
     return (
         <form onSubmit={handleBtn}>   {/* ✅ attach here */}
@@ -26,12 +48,18 @@ const Register = () => {
                         <div className="card-body">
                             <fieldset className="fieldset">
                                 <label className="label">Email</label>
-                                <input type="email" name='email' className="input" placeholder="Email" />
+                                <input type="email" name='email'  className="input" placeholder="Email" />
                                 <label className="label">Password</label>
                                 <input type="password" name='password' className="input" placeholder="Password" />
-                                <div><a className="link link-hover">Forgot password?</a></div>
                                 <button type="submit" className="btn btn-neutral mt-4">Register</button>
+                                {
+                                     error && <p  className='text-red-700'>{error}</p>
+                                }
+                                {
+                                    success && <p className='text-green-600'>User created successfully</p>
+                                }
                             </fieldset>
+                            <p>Already have an account ? Please <Link to = "/login" className='text-green-500 underline'>Login</Link></p>
                         </div>
                     </div>
                 </div>
